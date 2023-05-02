09d3f7518c39bce58bcc1b62882ee29408506c54
Tgc020202 <tgc142322@gmail.com>
2023-05-03 01:46:15 +0800

    upload some js

---
+++ Assests/JS/tgc.js
@@ -0,0 +1,45 @@
+// register
+function SignUpSuccess(){
+    var allInput = document.getElementsByTagName('input');
+    var detect = false;
+    for(var i = 0; i < allInput.length ;i++){
+        if(allInput[i].value == ''){
+            detect = true;
+            break;
+        }
+    }
+    if(detect){
+        alert('Please make sure you have filled all the blanks.');
+    }
+    else{
+        open("intro.html","_self");
+    }
+}
+
+// login
+
+function gotoSignUp(){
+    open("register.html","_self");
+}
+
+function SignInSuccess(){
+    var allInput = document.getElementsByTagName('input');
+    var detect = false;
+    for(var i = 0; i < allInput.length ;i++){
+        if(allInput[i].value == ''){
+            detect = true;
+            break;
+        }
+    }
+    if(detect){
+        alert('Please make sure you have filled all the blanks.');
+    }
+    else{
+        open("task_review.html","_self");
+    }
+}
+
+// feedback
+function FeedBackSuccess(){
+    close("_self");
+}
