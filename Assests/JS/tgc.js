d2ec36114c277ca9106871bf279b861f954025e5
Tgc020202 <tgc142322@gmail.com>
2023-05-04 17:43:53 +0800

    uploaded by tgc

--- Assests/JS/tgc.js
+++ Assests/JS/tgc.js
@@ -54,3 +54,28 @@ function FeedBackSuccess(){
 function gotoFeedBack(){
     open("feedback.html","_blank");
 }
+
+// Nav
+// collapse-side-nav-btn
+var toggle_side_btn = document.querySelector('.toggle-nav-btn');
+var sidebar = document.querySelector('.sidebar');
+var bg_semidark = document.querySelector('.bg-semidark');
+
+toggle_side_btn.onclick = function(){
+    sidebar.className = "sidebar d-block min-vh-100 col-auto position-fixed sidebar-show";
+    bg_semidark.style.display = 'block'
+    bg_semidark.onclick = function(){
+        this.style.display = "none";
+        sidebar.className = "sidebar d-none d-md-block min-vh-100 col-auto position-fixed";
+    };
+
+    const auto_close_sideNav = window.matchMedia('(min-width: 767px)');
+    function close(e){
+        if(e.matches){
+            sidebar.className = "sidebar d-none d-md-block min-vh-100 col-auto position-fixed";
+            bg_semidark.style.display = 'none';
+        }
+    };
+    auto_close_sideNav.addListener(close);
+    close(auto_close_sideNav);
+};
