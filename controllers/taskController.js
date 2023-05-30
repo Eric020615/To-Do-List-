bda848e32eaa4b055294478f227277b673a7163a
ericj <ericjuncheng10@gmail.com>
2023-05-30 11:51:12 +0800

    delete task, edit task functionality added

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -25,7 +25,6 @@ module.exports.task_to_do_get = async (req,res,next) =>{
     const user_id = user._id;
     try{
         let query = await Task.find({user_id: user_id});
-        console.log(query);
         res.locals.tasks = query;
         res.locals.date = currentDate;
         next();
@@ -45,7 +44,44 @@ module.exports.task_to_do_post = async (req,res) =>{
     }
     catch(err){
         const errors = handleErrors(err);
-        console.log(errors);
         res.status(400).json({errors});
     }
 }
+
+module.exports.task_delete = async (req,res) =>{
+    let {task_id} = req.body;
+    try{
+        const task = await Task.deleteOne({_id:task_id});
+        res.status(201).json({task});
+    }
+    catch(err){
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
+    }
+}
+
+module.exports.task_edit = async (req,res) =>{
+    let {task_id,title,description,date,progress_level} = req.body;
+    try{
+        const task_edited = await Task.findOneAndUpdate({_id:task_id},{title:title,description:description,date:date,progress_level:progress_level})
+        res.status(201).json({task_edited});
+        console.log({task_edited});
+    }
+    catch(err){
+        const errors = handleErrors(err);
+        res.status(400).json({errors});
+    }
+}
+
+// module.exports.task_done = async (req,res) =>{
+//     let {done} = req.body;
+//     try{
+//         const task_edited = await Task.findOneAndUpdate({_id:task_id},{progress_level:done})
+//         res.status(201).json({task_edited});
+//         console.log({task_edited});
+//     }
+//     catch(err){
+//         const errors = handleErrors(err);
+//         res.status(400).json({errors});
+//     }
+// }
