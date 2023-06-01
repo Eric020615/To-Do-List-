50ba53a37ca351f76ac44f02e9262b4904df9cca
HO KAI CHONG <feihaidao77@gmail.com>
2023-06-02 02:50:33 +0800

    alltask

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -49,3 +49,20 @@ module.exports.task_to_do_post = async (req,res) =>{
         res.status(400).json({errors});
     }
 }
+
+// All Task Get Data
+module.exports.all_task_get = async (req, res, next) => {
+  const user = res.locals.user;
+
+  let currentDate = new Date();
+  const user_id = user._id;
+  try {
+    const query = await Task.find({ user_id: user_id })
+    res.locals.tasks = query;
+    res.locals.date = currentDate;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
